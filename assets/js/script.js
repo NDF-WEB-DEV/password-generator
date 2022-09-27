2139d3d7e0969c2f6cebd55ded4f67d94f270f82
Nordleen De Frias <nordleendefrias@gmail.com>
2022-09-27 00:15:49 -0400

    Fixed duplicates on line 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // GLOBAL VARIABLES
 var generateBtn = document.querySelector("#generate");  // variable that will display the result in the HTML file
-var allCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","+","-","&&","||","!","(",")","{","}","[","]","^","~","*","?",":",];
+var allCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","+","-","&","|","!","(",")","{","}","[","]","^","~","*","?",":"];
 var password = document.getElementById("password");
 var randMix;
 
